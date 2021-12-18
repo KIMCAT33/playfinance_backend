66cd9d4b1c62d295e3279911f7d1651562d25feb
jdkim <jdkim@amazon.com>
2021-12-18 20:49:13 +0900

    startkit upadte

--- app.js
+++ app.js
@@ -3,29 +3,32 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 
 
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 connectDB();
-app.use(express.json({extneded: false}))
-
+app.use(express.json({extneded: false}));
 
 var cors = require('cors');
 app.use(cors());
 
 
 var indexRouter = require('./routes/index');
+var axieRouter = require('./routes/axies');
+var startkitRouter = require('./routes/startkit');
 //var homeRouter = require('./routes/home');
 //var recommendRouter = require('./routes/recommend');
-//var startkitRouter = require('./routes/startkit');
+
 //var airdropRouter = require('./routes/airdrop');
 
 
 app.use("/", indexRouter);
+app.use("/axies", axieRouter);
 //app.use("/home", homeRouter);
 //app.use("/recommend", recommendRouter);
-//app.use("/startkitRouter", startkitRouter);
+app.use("/startkit", startkitRouter);
 //app.use("/airdrop", airdropRouter);
 
 
